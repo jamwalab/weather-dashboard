1a5d2b9b7f755038399bf049dbe3351ec89a3ebb
Abhishek Jamwal <jamwal.ab@gmail.com>
2021-05-29 00:54:42 -0400

    weather display box

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var searchBox = document.querySelector(".search-input");
 var searchInput = document.querySelector("#searchCity");
-var currentWeather = document.querySelector(".current-weather");
+var currentWeather = document.querySelector("#current-weather");
+var forecast = document.querySelector(".forecast");
 var dateToday = moment().format("YYYY-MM-DD");
 console.log(dateToday);
 
@@ -67,7 +68,7 @@ var currentDisplay = function(data, city) {
     var wind = data.current.wind_speed;
     wind = (parseFloat(wind) * 3.6);
     var humidity = data.current.humidity;
-    var uvi = data.current.uvi;
+    var uvi = parseFloat(data.current.uvi);
     console.log(date,icon,temp,wind,humidity,uvi);
 
     currentWeather.textContent = "";
@@ -79,12 +80,29 @@ var currentDisplay = function(data, city) {
 
     var weatherDetails = document.createElement("article");
     weatherDetails.classList.add("m-2", "weatherDetails");
-    weatherDetails.innerHTML = "<p>Temp: "+temp+"°C</p><p>Wind: "+wind+" KPH</p><p>Humidity: "+humidity+" %</p><p>UV Index: <span class='uvi'> "+uvi+" </span></p>"
+    weatherDetails.innerHTML = "<p>Temp: "+temp+"°C</p><p>Wind: "+wind+" KPH</p><p>Humidity: "+humidity+" %</p><p>UV Index: <span id='uvi' class='px-3'> "+uvi+" </span></p>"
+   
     currentWeather.appendChild(cityDetails);
     currentWeather.appendChild(weatherDetails);
+
+    var uviColor = document.querySelector("#uvi");
+    if (uvi>=0 && uvi <3) {
+        uviColor.classList.add("badge", "badge-success")
+    } else if (uvi>=3 && uvi <6) {
+        uviColor.classList.add("badge", "badgeYellow")
+    } else if (uvi>=6 && uvi <8) {
+        uviColor.classList.add("badge", "badgeOrange")
+    } else if (uvi>=8 && uvi <11) {
+        uviColor.classList.add("badge", "badge-danger")
+    } else if (uvi>=11) {
+        uviColor.classList.add("badge", "badgeViolet")
+    } 
 }
 
 var weatherForecast = function(data) {
+    var counter = 0;
+    var forecastHead = document.createElement("h2");
+    forecastHead.textContent = "5-Day Forecast:";
     for (var i=0; i<data.daily.length; i++) {
         var date = moment((data.daily[i].dt)*1000).format("YYYY-MM-DD");
         var icon = "http://openweathermap.org/img/w/"+data.daily[i].weather[0].icon+".png";
@@ -93,6 +111,11 @@ var weatherForecast = function(data) {
         var humidity = data.daily[i].humidity;
         var uvi = data.daily[i].uvi;
         console.log(date,icon,temp,wind,humidity,uvi);
+
+        if (date > dateToday && counter <5) {
+            console.log(counter);
+            counter++;
+        }
     }
     
 }
