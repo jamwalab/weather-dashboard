738a3a2b7b225bd45cdf02ab3f0b70bc35a9e43b
Abhishek Jamwal <jamwal.ab@gmail.com>
2021-05-29 00:22:35 -0400

    current weather displayed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,47 @@
 var searchBox = document.querySelector(".search-input");
 var searchInput = document.querySelector("#searchCity");
+var currentWeather = document.querySelector(".current-weather");
 var dateToday = moment().format("YYYY-MM-DD");
 console.log(dateToday);
+
+var warning = function() {
+    currentWeather.classList.remove("border","border-dark");
+    currentWeather.textContent = "City not found, please try again!!";
+    currentWeather.classList.add("warning");
+}
 
 var weatherSearch = function(event) {
     event.preventDefault();
     var city = searchInput.value;
+    searchInput.value = "";
     console.log(city);
     fetch(
         "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=71caaa193e9262e0eb4c901abdadf9c8&units=metric"
     ).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
+                console.log(data)
                 var lat = data.coord.lat;
                 var lon = data.coord.lon;
+                var cityName = data.name + ", " + data.sys.country;
                 fetch(
                     "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=minutely,hourly,alerts&appid=71caaa193e9262e0eb4c901abdadf9c8&units=metric"
                 ).then(function(response) {
                     if (response.ok) {
                         response.json().then(function(data) {
                             console.log(data);
-                            currentWeather(data);
+                            currentDisplay(data, cityName);
                             weatherForecast(data);
                         })   
                     }
                     else {
-                        console.log("City not found. Please try again")
+                        warning();
                     }
                 })
             })
         }
         else {
-            console.log("City not found. Please try again")
+            warning();
         }
     })
     /*fetch(
@@ -50,14 +60,28 @@ var weatherSearch = function(event) {
     })*/
 }
 
-var currentWeather = function(data) {
+var currentDisplay = function(data, city) {
     var date = moment((data.current.dt)*1000).format("YYYY-MM-DD");
     var icon = "http://openweathermap.org/img/w/"+data.current.weather[0].icon+".png";
     var temp = data.current.temp;
     var wind = data.current.wind_speed;
+    wind = (parseFloat(wind) * 3.6);
     var humidity = data.current.humidity;
     var uvi = data.current.uvi;
     console.log(date,icon,temp,wind,humidity,uvi);
+
+    currentWeather.textContent = "";
+    currentWeather.classList.remove("warning");
+    currentWeather.classList.add("border","border-dark");
+    var cityDetails = document.createElement("h2");
+    cityDetails.classList.add("m-2");
+    cityDetails.innerHTML = city + "  " + " ("+date+") " + "<img src='"+icon+"' alt='icon'/>"
+
+    var weatherDetails = document.createElement("article");
+    weatherDetails.classList.add("m-2", "weatherDetails");
+    weatherDetails.innerHTML = "<p>Temp: "+temp+"°C</p><p>Wind: "+wind+" KPH</p><p>Humidity: "+humidity+" %</p><p>UV Index: <span class='uvi'> "+uvi+" </span></p>"
+    currentWeather.appendChild(cityDetails);
+    currentWeather.appendChild(weatherDetails);
 }
 
 var weatherForecast = function(data) {
